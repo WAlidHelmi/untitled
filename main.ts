6bc022e27bcb5739237e082b7123799196e71663
WAlidHelmi <113910191+WAlidHelmi@users.noreply.github.com>
2022-11-08 16:15:19 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (list[0]))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (list[10]))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (list[3]))
+})
+let list: string[] = []
+list = [
+"Computer",
+"disk",
+"table",
+"chair",
+"phone",
+"Hello",
+"math",
+"programing",
+"version",
+"new"
+]
